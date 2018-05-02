563d523eb59f49118b0e1e50efbbefe240cb30d0
jiangxs <songxjxs@yeah.net>
2018-05-02 15:01:54 +0800

    设计模式的工程

---
+++ src/com/jxs/simpleFactory/Operation.java
@@ -0,0 +1,12 @@
+package com.jxs.simpleFactory;
+
+/**
+ * Created by jiangxs on 2018/5/2.
+ */
+public abstract class Operation {
+
+    private double numberA = 0.0;
+    private double numberB = 0.0;
+
+    public double getResult;
+}
---
+++ src/com/jxs/simpleFactory/OperationAdd.java
@@ -0,0 +1,9 @@
+package com.jxs.simpleFactory;
+
+/**
+ * Created by jiangxs on 2018/5/2.
+ */
+public class OperationAdd extends Operation {
+
+
+}
