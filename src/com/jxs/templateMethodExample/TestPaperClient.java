c7a94a68b839206438128888ca96a714eb7ebad2
Jiangxs <songxjxs@yeah.ent>
2018-05-08 07:52:34 +0800

    模板方法类客户端的修改

--- src/com/jxs/templateMethodExample/TestPaperClient.java
+++ src/com/jxs/templateMethodExample/TestPaperClient.java
@@ -7,7 +7,7 @@ public class TestPaperClient {
 
     public static void main(String[] args) {
 
-        System.out.println("学生甲抄的试卷：");
+        System.out.println("学生甲做的试卷：");
         TestPaperA studentA = new TestPaperA();
         studentA.testQuestion1();
         studentA.testQuestion2();
@@ -15,7 +15,7 @@ public class TestPaperClient {
 
         System.out.println();
 
-        System.out.println("学生乙抄的试卷：");
+        System.out.println("学生乙做的试卷：");
         TestPaperB studentB = new TestPaperB();
         studentB.testQuestion1();
         studentB.testQuestion2();
