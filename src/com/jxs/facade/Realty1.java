b8ea47cfa9f50a294f212666663988406570934e
Jiangxs <songxjxs@yeah.ent>
2018-05-07 11:15:31 +0800

    增加外观模式

---
+++ src/com/jxs/facade/Realty1.java
@@ -0,0 +1,21 @@
+package com.jxs.facade;
+
+/**
+ * Created by jiangxs on 2018/5/7.
+ * <p>
+ * 房地产1
+ */
+public class Realty1 {
+
+    // 卖房地产
+    public void sell() {
+
+        System.out.println("房地产1卖出");
+    }
+
+    // 买房地产
+    public void buy() {
+
+        System.out.println("房地产1买入");
+    }
+}
